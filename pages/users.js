c27fa33cc8fd7617667b04a40016054c4862d804
rinchndgr <101038499+rinchndgr@users.noreply.github.com>
2023-03-01 20:04:55 +0800

    3 files modified

--- pages/users.js
+++ pages/users.js
@@ -1,10 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import Avatar, { genConfig } from 'react-nice-avatar';
+import { Triangle } from 'react-loader-spinner';
+
+// useEffect -> pageiig hereglegch duudah vued hamgiin ehend duudagna
 
 function Users() {
   const [users, setUsers] = useState([]);
+
+  // ogogdol tatah veiin tolov
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((result) => result.json())
+      .then((data) => {
+        console.log('Data', data);
+        setUsers(data);
+        setLoading(false);
+      });
+    // console.log('Use Effect');
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="w-full h-screen flex justify-center items-center">
+        <Triangle
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      </section>
+    );
+  }
+
   return (
-    <section>
-      <h1>Users {users.length}</h1>
+    <section className="p-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-full">
+      {users &&
+        users.length > 0 &&
+        users.map((user) => {
+          const config = genConfig(user.email);
+          return (
+            <div className="p-5 cursor-pointer hover:bg-gray-200 hover:rounded-lg">
+              <Avatar className="w-32 h-32" {...config} />
+              <h1 className="mt-4">{user.name}</h1>
+              {/* <h3>Email: {user.email}</h3> */}
+            </div>
+          );
+        })}
     </section>
   );
 }
